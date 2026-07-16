d2dfd9351b2b0d5fb97581eceb9f9e4af30fc201
ThematicallyUnpossible <fernando_linux@proton.me>
2026-07-16 23:27:35 +0700

    +search pid from process name

---
+++ build/CMakeFiles/libloader.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libloader.dir/link.d"
+  "CMakeFiles/libloader.dir/main.cpp.o"
+  "CMakeFiles/libloader.dir/main.cpp.o.d"
+  "libloader"
+  "libloader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libloader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/libloader.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for libloader.
+# This may be replaced when dependencies are built.
